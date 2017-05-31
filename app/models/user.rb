b5643bc9215f366984c86d551e52752add8465c1
Eddie Jou <eddiejou@users.noreply.github.com>
2017-05-31 11:42:51 -0500

    users.rb has corrected validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
+
+  #Associations
+
+  has_many :likes
+  has_many :photos
+  has_many :comments
+
+  has_many :liked_photos, :through => :likes, :source => :photo
+
+  #Validations
+  validates :username, :presence => true, :uniqueness => true
 end
